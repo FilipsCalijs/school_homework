17357c41f84554ec9463723dbc0a2465b65403ce
FilipsCalijs <filips.calijs@gmail.com>
2024-10-13 12:53:50 +0300

    fix some bags

--- js/bar/script.js
+++ js/bar/script.js
@@ -1,12 +1,51 @@
-// Найти кнопку переключения
-const darkModeToggle = document.getElementById('darkModeToggle');
+const toggleButton = document.getElementById('darkModeToggle');
+const hamburger = document.getElementById("hamburger");
+const navList = document.querySelector(".nav-list");
+const modal = document.getElementById("modal");
+const modalBtn = document.getElementById("modalBtn");
+const closeBtn = document.querySelector(".close");
 
-// Обработчик события нажатия
-darkModeToggle.addEventListener('click', function(e) {
-    e.preventDefault(); // предотвращаем переход по ссылке
+function toggleDarkMode() {
+    if (toggleButton.textContent.includes('Dark mode')) {
+        toggleButton.innerHTML = '<i class="fas fa-home"></i> Light mode';
+        document.body.classList.add('dark-mode', 'dark-theme'); 
+    } else {
+        toggleButton.innerHTML = '<i class="fas fa-home"></i> Dark mode';
+        document.body.classList.remove('dark-mode', 'dark-theme'); 
+    }
+}
 
-    // Переключаем класс темного режима на body и nav
-    document.body.classList.toggle('dark-mode');
-    document.querySelector('nav').classList.toggle('dark-mode');
-});
-console.log("12");
+function toggleMenu() {
+    navList.classList.toggle("active");
+    hamburger.classList.toggle("open");
+}
+
+function handleResize() {
+    if (window.innerWidth > 770) {
+        navList.classList.remove("active");
+        hamburger.classList.remove("open");
+    }
+}
+
+function openModal(event) {
+    event.preventDefault();
+    modal.style.display = "block";
+    event.stopPropagation();
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+function outsideClick(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+toggleButton.addEventListener('click', toggleDarkMode);
+hamburger.addEventListener("click", toggleMenu);
+window.addEventListener("resize", handleResize);
+modalBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
