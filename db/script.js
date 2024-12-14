bd1e46daff04f379f7025c9e1730545700656323
FilipsCalijs <filips.calijs@gmail.com>
2024-12-14 23:23:16 +0200

    new nav

--- db/script.js
+++
@@ -1,9 +0,0 @@
-function validateRegister() {
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm-password").value;
-    if (password !== confirmPassword) {
-        alert("Passwords do not match!");
-        return false;
-    }
-    return true;
-}
